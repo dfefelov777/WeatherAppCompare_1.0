4a9ab9b0592c95fa713139e891f75e092c57070f
DFEFELOV01 <dima.fefelov97.fefelov@mail.ru>
2024-08-02 21:44:39 +0500

    fix

--- Weather_App_Django/weather_project/weather_app/views.py
+++ Weather_App_Django/weather_project/weather_app/views.py
@@ -9,22 +9,26 @@ def index(request):
     forecast_url = 'https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=current,minutely,hourly,alerts&appid={}'
 
     if request.method == 'POST':
-        city1 = request.POST['city1']
-        city2 = request.POST.get('city2', None)
+        city1 = request.POST.get('city1', '').strip()
+        city2 = request.POST.get('city2', '').strip()
 
-        weather_data1, daily_forecasts1 = fetch_weather_and_forecast(city1, api_key, current_weather_url, forecast_url)
+        weather_data1, daily_forecasts1 = None, None
+        weather_data2, daily_forecasts2 = None, None
+        error_message1, error_message2 = None, None
+
+        if city1:
+            weather_data1, daily_forecasts1, error_message1 = fetch_weather_and_forecast(city1, api_key, current_weather_url, forecast_url)
 
         if city2:
-            weather_data2, daily_forecasts2 = fetch_weather_and_forecast(city2, api_key, current_weather_url,
-                                                                         forecast_url)
-        else:
-            weather_data2, daily_forecasts2 = None, None
+            weather_data2, daily_forecasts2, error_message2 = fetch_weather_and_forecast(city2, api_key, current_weather_url, forecast_url)
 
         context = {
             'weather_data1': weather_data1,
             'daily_forecasts1': daily_forecasts1,
             'weather_data2': weather_data2,
             'daily_forecasts2': daily_forecasts2,
+            'error_message1': error_message1,
+            'error_message2': error_message2,
         }
 
         return render(request, 'weather_app/index.html', context)
@@ -37,6 +41,9 @@ def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url)
     for attempt in range(max_retries):
         try:
             response = requests.get(current_weather_url.format(city, api_key)).json()
+            if response.get('cod') != 200:
+                return None, None, response.get('message', 'Error fetching data')
+
             lat, lon = response['coord']['lat'], response['coord']['lon']
             forecast_response = requests.get(forecast_url.format(lat, lon, api_key)).json()
 
@@ -58,8 +65,8 @@ def fetch_weather_and_forecast(city, api_key, current_weather_url, forecast_url)
                         'icon': daily_data['weather'][0]['icon'],
                     })
 
-            return weather_data, daily_forecasts
+            return weather_data, daily_forecasts, None
         except requests.exceptions.RequestException as e:
             print(f"Attempt {attempt + 1} failed: {e}")
-            time.sleep(2)  # Задержка перед повторной попыткой
-    return None, None
+            time.sleep(2)  # Задержка перед повторной попыткой, чтобы приложение не падало
+    return None, None, 'Failed to fetch data after multiple attempts'
